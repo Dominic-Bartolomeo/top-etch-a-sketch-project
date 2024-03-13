f741572a63888b256ea2084749bdbfebc6600279
Dominic-Bartolomeo <116409215+Dominic-Bartolomeo@users.noreply.github.com>
2024-03-12 21:10:50 -0400

    Add grid from clicking button

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const btn = document.querySelector("#btn");
 
-let input = 0;
+let input = 16;
 
 // Create grid
 function createGrid(input) {
     // Create input,input grid
+    const gridContainer = document.querySelector("#container");
+    let gridHW = (960 / input);
+    for (let i = 0; i < input; i++){
+        const gridDiv = document.createElement("div");
+        gridDiv.id = "grid";
+        gridDiv.style.backgroundColor = "blue";
+        gridDiv.style.width = ""+gridHW+"px";
+        gridDiv.style.minHeight = ""+gridHW+"px";
+        gridContainer.appendChild(gridDiv);
+    }
 }
 
 btn.addEventListener("click", () => {
@@ -12,5 +22,6 @@ btn.addEventListener("click", () => {
     while (input > 100 || input < 2 || isNaN(input)) {
         input = prompt("The number must be between 2-100. Try again.");
     }
+    createGrid(input);
 });
 
