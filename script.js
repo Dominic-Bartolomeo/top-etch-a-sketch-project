7eac041fc03e50d5e6cc99549e10d3224298d6b7
Dominic-Bartolomeo <116409215+Dominic-Bartolomeo@users.noreply.github.com>
2024-03-12 22:50:38 -0400

    Add grid creation functionality

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
 const btn = document.querySelector("#btn");
 
-let input = 16;
+// let input = 16;
 
 // Create grid
 function createGrid(input) {
     // Create input,input grid
+    let realInput = input++;
     const gridContainer = document.querySelector("#container");
-    let gridHW = (960 / input);
-    for (let i = 0; i < input; i++){
-        const gridDiv = document.createElement("div");
-        gridDiv.id = "grid";
-        gridDiv.style.backgroundColor = "blue";
-        gridDiv.style.width = ""+gridHW+"px";
-        gridDiv.style.minHeight = ""+gridHW+"px";
-        gridContainer.appendChild(gridDiv);
+    let gridWidth = (960 / input);
+    // console.log("RealInput:" + realInput);
+    console.log("Before rounding:" + gridWidth + "|" + input + "/" + gridWidth);
+    gridWidth = Math.round(gridWidth);
+    console.log("After rounding:" + gridWidth + "|" + input + "/" + gridWidth);
+    for(let i = 0; i<realInput;i++)
+        for (let j = 0; j < realInput; j++){
+            const gridDiv = document.createElement("div");
+            gridDiv.id = "grid";
+            gridDiv.style.backgroundColor = "blue";
+            gridDiv.style.width = ""+gridWidth+"px";
+            gridDiv.style.border = "1px solid";
+            gridDiv.style.borderColor = "black";
+            gridContainer.appendChild(gridDiv);
+        }
     }
-}
 
 btn.addEventListener("click", () => {
     let input = prompt("Enter number of squares on each side (max 100).");
     while (input > 100 || input < 2 || isNaN(input)) {
         input = prompt("The number must be between 2-100. Try again.");
     }
-    createGrid(input);
+    intInput = parseInt(input);
+    createGrid(intInput);
 });
 
